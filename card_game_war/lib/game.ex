28e9681001a0cfa917b03eb5dfba6cd7c48152bb
Agustín Ramos Fonseca <zentimental.software@gmail.com>
2015-10-25 05:02:04 -0600

    Card game war kata. 1st version.

---
+++ card_game_war/lib/game.ex
@@ -0,0 +1,14 @@
+defmodule Game do
+
+  # feel free to use these cards or use your own data structure"
+  def suits, do: [:spade, :club, :diamond, :heart]
+  def ranks, do: [2, 3, 4, 5, 6, 7, 8, 9, 10, :jack, :queen, :king, :ace]
+  def cards do
+    for suit <- suits,
+        rank <- ranks do
+      {suit, rank}
+    end
+  end
+
+
+end
---
+++ card_game_war/test/game_test.exs
@@ -0,0 +1,36 @@
+defmodule CardGameWarTest do
+  use ExUnit.Case
+
+  test "the highest rank wins the cards in the round" do
+    flunk("not implemented")
+  end
+
+  test "queens are higher rank than jacks" do
+    flunk("not implemented")
+  end
+
+  test "kings are higher rank than queens" do
+    flunk("not implemented")
+  end
+
+  test "aces are higher rank than kings" do
+    flunk("not implemented")
+  end
+
+  test "if the ranks are equal, clubs beat spades" do
+    flunk("not implemented")
+  end
+
+  test "if the ranks are equal, diamonds beat clubs" do
+    flunk("not implemented")
+  end
+
+  test "if the ranks are equal, hearts beat diamonds" do
+    flunk("not implemented")
+  end
+
+  test "the player loses when they run out of cards" do
+    flunk("not implemented")
+  end
+
+end
